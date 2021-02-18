3d398bce8535c6c61877199fbdb6a2650f3d65da
Hubert Traschwandtner <75886767+Hubert-Trasch@users.noreply.github.com>
2021-02-18 17:17:36 +0100

    Add files via upload

---
+++ Lab_intro_sql.sql
@@ -0,0 +1,14 @@
+select title from film;
+
+select name as film_language from language;
+
+select sum(store_id)-1 from store;
+
+select sum(store_id)-1 from store;
+
+select sum(id)-1 from staff_list;
+select count(id) from staff_list;
+
+select first_name from staff;
+
+select rental_duration from film;
